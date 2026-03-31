3bcac0fd41d3879d75f43362c0572b59f1db813e
Kz-Himel <kzhimel129@gmail.com>
2026-03-31 22:40:00 +0600

    Fetching Data from data.json

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,14 @@ import Navbar from './components/Navbar/Navbar'
 import Banner from './components/Banner/Banner'
 import Stats from './components/Stats/Stats'
 
+const dataFetching = async () => {
+  const res = await fetch("/data.json");
+  return res.json();
+};
+
 function App() {
+
+  const dataPromise = dataFetching();
 
   return (
     <>
